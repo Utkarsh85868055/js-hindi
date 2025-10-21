46dfa978018b0b19a3621d7cacaa271a6fd3c3f9
Utkarsh85868055 <Utkarsh8555@gmail.com>
2025-10-21 13:48:13 +0000

    conversion of datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,6 +1,6 @@
 "use strict"; // treat all JS code as newer version 
 
-// alert (3 + 3 ) // we are using node js not browser
+// alert (3 + 3 ) // we are using node js, not browser
 
 console.log(3 + 3)
  //code readability should be high
@@ -18,8 +18,11 @@ let isLoggedIn = false
 // boolean
 // null = standalone value
 // undefined =>
-// symbol = uniqueness
+// symbol => uniqueness
 
 // object 
 
-console.log(typeof null); 
+console.log(typeof null); // object
+
+console.log(typeof undefined); //undefined
+
